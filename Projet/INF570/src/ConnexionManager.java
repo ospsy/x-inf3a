8c6259f094e8ad78820d72cdd23c36498c643a8f
seg.benoit <seg.benoit@731a3307-b972-3c27-4b66-319fd9e8ed1f>
2010-11-09 14:06:56 +0000

    Premières bases du ConnexionManager

--- Projet/INF570/src/ConnexionManager.java
+++ Projet/INF570/src/ConnexionManager.java
@@ -1,9 +1,12 @@
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.concurrent.ConcurrentSkipListSet;
 
 /**
- * Contient la liste des connexions et g�re les innondations
- * @author Julio
+ * La classe ConnexionManager gère les relations entre les différentes Connexions,
+ * comme l'envoi global, les déconnexions et les confirmations de connexions.
+ * @see Connexion
+ * @author Julien, Malik, Benoit X08
  *
  */
 public class ConnexionManager extends Thread{
@@ -20,22 +23,56 @@ public class ConnexionManager extends Thread{
 		this.preConnexions = new HashMap<Connexion, Connexion>();
 	}
 	
-	public synchronized void remove(Connexion c){}
-	public synchronized void addConnexion(Connexion preConnexion){}
-
-
-	public synchronized void sendAll(Message m, Connexion exclude){
-		
-	}
 	/**
-	 * Extrait le premier message de la file d'envoi de mani�re synchronis�e TODO
-	 * @return le message � envoyer
+	 * Enlève une connexion de la liste des connexions gérées
+	 * @param c la connexion à enlever
 	 */
-	private synchronized Message getFirstToSend(){
-		return null;
+	public synchronized void remove(Connexion c){
+		connexions.remove(c);
 	}
 	
+	/**
+	 * Ajoute une connexion à confirmer à la liste des preConnexions gérées
+	 * @param preConnexion la connexion à confirmer
+	 */
+	public synchronized void addConnexion(Connexion preConnexion){
+		preConnexions.put(preConnexion, preConnexion);
+	}
+
+	/**
+	 * Envoit un message à toutes les connexions gérées sauf 
+	 * @param m le message à envoyer
+	 * @param exclude la connexion à exclure
+	 */
+	public synchronized void sendAll(Message m, Connexion exclude){
+		for(Connexion c : connexions.keySet()){
+			if(c!=exclude){
+				c.send(m);
+			}
+		}
+	}
+	
+	/**
+	 * Extrait le premier message de la file d'envoi de manière synchronisée
+	 * @return le message à envoyer
+	 */
+	private synchronized Message getFirstToSend(){
+		if(toSend.size()>0)
+			return toSend.removeFirst();
+		else 
+			return null;
+	}
+	
+	/**
+	 * Confirme la connexion dans la liste globale
+	 * @param c la connexion à confirmer
+	 */
 	private synchronized void confirmConnexion(Connexion c){
+		if(preConnexions.remove(c)!=null){
+			connexions.put(c, c);
+		}else{//si la connexion n'était pas dans la liste de preConnexion
+			System.err.println("confirmConnexion : la connexion n'était pas à confirmer...");
+		}
 		
 	}
 	
